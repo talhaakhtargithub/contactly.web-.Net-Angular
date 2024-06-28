9916a64c38408ab12326e5e8bf9fcac0762c68e9
Talha Akhtar <talhaakhtarkpr1@gmail.com>
2024-06-28 20:18:48 +0500

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Contact } from '../models/contact.model';
+import { AsyncPipe } from '@angular/common';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,AsyncPipe,HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'contactly.web';
+  http=inject(HttpClient);
+  contactsForm=new FormGroup({
+    name:new FormControl<string>(''),
+    email:new FormGroup<string | null>(null),
+    phone:new FormControl<string>(''),
+    favorite:new FormControl<boolean>(false)
+  })
+
+  contacts$=this.getContacts();
+
+  private getContacts(): Observable<Contact[]> {
+    return this.http.get<Contact[]>('https://localhost:7059/api/Contacts')
+  }
 }
+
+
